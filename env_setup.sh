a4d751f654e41c0d059eca4579e6c4e540301038
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-02-08 13:27:24 -0800

    changed some relative paths in env_setup

--- env_setup.sh
+++ env_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PYTHONPATH=../ramcloud/bindings/python
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../ramcloud/obj.master
+export PYTHONPATH=../../ramcloud/bindings/python
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../ramcloud/obj.master
 
