4e798f4d995d540aa9e7d6808d08542339af522d
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-02-12 15:29:49 -0800

    made env_setup start the sever and coordinator too

--- env_setup.sh
+++ env_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PYTHONPATH=../../ramcloud/bindings/python
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../ramcloud/obj.master
-
+../ramcloud/obj.master/coordinator > coordinator.out 2>&1 &
+../ramcloud/obj.master/server -M -r 0 > server.out 2>&1 &
