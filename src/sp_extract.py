15e2406c00e69df16b289136b55506a298923b79
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-02-13 15:23:24 -0800

    wrote a program to read out the shortest path results from the graph nodes in ramcloud

---
+++ src/sp_extract.py
@@ -0,0 +1,24 @@
+import sys
+import ramcloud
+
+GRAPH_TABLE_NAME = "graph"
+COORDINATOR_LOCATION = "fast+udp:host=127.0.0.1,port=12246"
+
+def main():
+    # get connected to ramcloud
+    rc = ramcloud.RAMCloud()
+    rc.connect(COORDINATOR_LOCATION)
+
+    graph_tableid = rc.get_table_id(GRAPH_TABLE_NAME)
+  
+    start_key = sys.argv[1]
+    end_key = sys.argv[2]
+    for nodeid in range(int(start_key), int(end_key) + 1):
+      try:
+        value, version = rc.read(graph_tableid, str(nodeid))
+        print str(nodeid) + ": " + value.split(' ')[-1]
+      except:
+        pass
+
+if __name__ == '__main__':
+    main()
