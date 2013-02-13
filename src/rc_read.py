7c1efd9bcbe1da921d8c543cdaeafd372c045105
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-02-13 15:24:47 -0800

    a helper script to read single key,value pairs out of ramcloud

---
+++ src/rc_read.py
@@ -0,0 +1,20 @@
+import sys
+import ramcloud
+
+GRAPH_TABLE_NAME = "graph"
+COORDINATOR_LOCATION = "fast+udp:host=127.0.0.1,port=12246"
+
+def main():
+    # get connected to ramcloud
+    rc = ramcloud.RAMCloud()
+    rc.connect(COORDINATOR_LOCATION)
+
+    graph_tableid = rc.get_table_id(GRAPH_TABLE_NAME)
+  
+    key = sys.argv[1]
+    value, version = rc.read(graph_tableid, key)
+
+    print key + ": " + value
+    
+if __name__ == '__main__':
+    main()
