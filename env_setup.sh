fc7947aed02c4751934d8ac9703e993af100a572
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-02-25 16:26:28 -0800

    removing server and coordinator spin-ups from env_setup

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=../../ramcloud/bindings/python
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../ramcloud/obj.master
-../ramcloud/obj.master/coordinator > coordinator.out 2>&1 &
-../ramcloud/obj.master/server -M -r 0 > server.out 2>&1 &
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../ramcloud/obj.master:../../local/lib
+export PATH=$PATH:/home/jdellit/local/bin
