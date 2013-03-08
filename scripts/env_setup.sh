0079ed3cc5dbfc726377c3e689d8d485eda077c9
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-03-07 17:03:06 -0800

    updated the env_setup and rc_spinup scripts to point to the right coordinator and server and also to start them with the parameters necessary for them to support remote reading and writing to ramcloud by clients

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PYTHONPATH=../../ramcloud/bindings/python
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../ramcloud/obj.master:../../local/lib
-export PATH=$PATH:/home/jdellit/local/bin
+export PYTHONPATH=~/ramcloud/bindings/python
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/ramcloud/obj.master:~/local/lib
+export PATH=$PATH:~/local/bin
