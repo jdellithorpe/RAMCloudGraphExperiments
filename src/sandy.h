a18aab2be304a69c5cce18c0a51be89472d17e8b
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-03-20 15:27:34 -0700

    Corresponding changes to the header file related to the last commit

--- src/sandy.h
+++ src/sandy.h
@@ -5,4 +5,7 @@
 #define DIST_TABLE_NAME "dist"
 #define COORDINATOR_LOCATION "infrc:host=192.168.1.101,port=12247"
 
+#define MULTIWRITE_REQ_MAX (size_t)64
+#define MULTIREAD_REQ_MAX (size_t)8
+
 #endif // SANDY_SANDY_H
