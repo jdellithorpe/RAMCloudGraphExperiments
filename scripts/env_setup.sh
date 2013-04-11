a03e4851e1293b78d3244fe383369d4ac87285bb
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-04-11 14:54:41 -0700

    updated environment variable settings

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PYTHONPATH=~/ramcloud/bindings/python
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/ramcloud/obj.master:~/local/lib
-export PATH=$PATH:~/local/bin
+export LD_LIBRARY_PATH=~/ramcloud/obj.master:~/local/lib:$LD_LIBRARY_PATH
+export PATH=~/local/bin:~/prebuilt/apache-ant-1.9.0/bin:~/prebuilt/apache-maven-3.0.5/bin:~/source/hadoop-0.20.203.0/bin:$PATH
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.9.x86_64/jre
