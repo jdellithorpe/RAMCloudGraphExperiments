73fa1d7673e4a278bcf7fe53e73ddb1254e38e02
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-04-25 10:19:50 -0700

    Modified reader to use multiWrites

--- src/reader.cc
+++ src/reader.cc
@@ -26,13 +26,15 @@ int main(int argc, char* argv[]) {
   string input_file;
   uint32_t server_span = 1;
   bool verbose = false;
+  uint32_t multiwrite_size = MULTIWRITE_REQ_MAX;
 
   desc.add_options()
       ("help", "produce help message")
       ("verbose", "print additional messages")
       ("input_format", po::value<string>(&input_format)->required(), "specify the input file format (adj_list, edge_list)")
       ("input_file", po::value<string>(&input_file)->required(), "specify the input file")
-      ("server_span", po::value<uint32_t>(&server_span), "set the number of servers to spread the table across (default 1)" );
+      ("server_span", po::value<uint32_t>(&server_span), "set the number of RAMCloud servers to spread the table across (default 1)" )
+      ("multiwrite_size", po::value<uint32_t>(&multiwrite_size), "set the number of objects to batch together in a write to RAMCloud (default/max 64)");
 
   po::variables_map vm;
 
@@ -117,35 +119,65 @@ int main(int argc, char* argv[]) {
     string adj_str;
     std::vector<string> adj_vec;
     AdjacencyList adj_list_pb;
-    string adj_list_str;
-    string src;
+    string adj_list_str[MULTIWRITE_REQ_MAX];
+    string src[MULTIWRITE_REQ_MAX];
     uint64_t node_write_count = 0;
     uint64_t bytes_written = 0;
+    uint64_t stat_time_write_start;
+    uint64_t stat_time_write_acc = 0;
+    size_t multiwrite_queue_size = 0;
+    MultiWriteObject* requests[MULTIWRITE_REQ_MAX];
+    Tub<MultiWriteObject> objects[MULTIWRITE_REQ_MAX];
     while(std::getline(graph_filestream, adj_str)) {
       boost::split(adj_vec, adj_str, boost::is_any_of(" "));
-      src = adj_vec[0];
-      for(int i = 1; i<adj_vec.size(); i++) {
-        adj_list_pb.add_neighbor(boost::lexical_cast<uint64_t>(adj_vec[i]));
-      }
-      if(adj_list_pb.neighbor_size() > 0) {
-        adj_list_pb.SerializeToString(&adj_list_str);
-        try {
-          client.write( graph_tableid,
-                        src.c_str(),
-                        src.length(),
-                        adj_list_str.c_str(),
-                        adj_list_str.length() );
-          node_write_count++;
-          bytes_written += adj_list_str.length();
-          if(verbose && (node_write_count%100000)==0)
-              std::cout << prog_name << ": INFO: wrote " << node_write_count << "th vertex to RAMCloud, total of " << bytes_written << "B written\n";
-        } catch (RAMCloud::ClientException& e) {
-          fprintf(stderr, "RAMCloud exception: %s\n", e.str().c_str());
-          return 1;
+      if(adj_vec.size() > 1) {
+        src[multiwrite_queue_size] = adj_vec[0];
+        for(int i = 1; i<adj_vec.size(); i++) {
+          adj_list_pb.add_neighbor(boost::lexical_cast<uint64_t>(adj_vec[i]));
         }
+        adj_list_pb.SerializeToString(&adj_list_str[multiwrite_queue_size]);
+
+        objects[multiwrite_queue_size].construct( graph_tableid,
+                                                  src[multiwrite_queue_size].c_str(),
+                                                  src[multiwrite_queue_size].length(),
+                                                  adj_list_str[multiwrite_queue_size].c_str(),
+                                                  adj_list_str[multiwrite_queue_size].length() );
+        requests[multiwrite_queue_size] = objects[multiwrite_queue_size].get();
+
+        node_write_count++;
+        bytes_written += adj_list_str[multiwrite_queue_size].length();
+        multiwrite_queue_size++;
+
+        if(multiwrite_queue_size == multiwrite_size) {
+          stat_time_write_start = Cycles::rdtsc();
+          try {
+            client.multiWrite(requests, multiwrite_queue_size);
+          } catch(RAMCloud::ClientException& e) {
+            fprintf(stderr, "RAMCloud exception: %s\n", e.str().c_str());
+            return 1;
+          }
+          stat_time_write_acc += Cycles::rdtsc() - stat_time_write_start;
+
+          multiwrite_queue_size = 0;
+        }
+
+        if(verbose && (node_write_count%100000)==0)
+          std::cout << prog_name << ": INFO: wrote " << node_write_count << "th vertex to RAMCloud, total of " << bytes_written << "B written @ " << bytes_written/Cycles::toSeconds(stat_time_write_acc) << "B/s, " << Cycles::toMicroseconds(stat_time_write_acc)/(double)node_write_count << "us/node\n";
       }
       adj_list_pb.clear_neighbor();
     }
+
+    if(multiwrite_queue_size != 0) {
+      try {
+        client.multiWrite(requests, multiwrite_queue_size);
+      } catch(RAMCloud::ClientException& e) {
+        fprintf(stderr, "RAMCloud exception: %s\n", e.str().c_str());
+        return 1;
+      }
+
+      multiwrite_queue_size = 0;
+    }
+
   } else {
     fprintf(stderr, "ERROR: Unrecognized value for input_format program option: %s\n", input_format.c_str());
     return -1;
