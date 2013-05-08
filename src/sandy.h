e295d9692e5923d1e4b67e2f1afd30d1062d2724
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-05-08 11:22:45 -0700

    Corresponding changes to sandy.h

--- src/sandy.h
+++ src/sandy.h
@@ -7,8 +7,10 @@
 #define REPORTER_NAP_TIME 10
 
 #define MULTIWRITE_REQ_MAX (size_t)400
-#define MULTIREAD_REQ_MAX (size_t)64
+#define MULTIREAD_REQ_MAX (size_t)1000
 
 #define FRONTIER_EDGE_Q_SIZE_MAX (size_t)1000000
+
+#define EDGE_BATCH_MAX (size_t)1000
 
 #endif // SANDY_SANDY_H
