3ce3a51fd4277202a052f86a20b75939db260572
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-05-21 13:28:31 -0700

    Separated sandy.h functionality into sandy.h for all projects, and a header file (now only mt_bfs.h) for specific applications.

---
+++ src/mt_bfs.h
@@ -0,0 +1,13 @@
+#ifndef SANDY_MTBFS_H // <PROJECT>_<PATH>_<FILE>_H
+#define SANDY_MTBFS_H
+
+#define REPORTER_NAP_TIME 10
+
+#define MULTIWRITE_REQ_MAX (size_t)400
+#define MULTIREAD_REQ_MAX (size_t)1000
+
+#define FRONTIER_EDGE_Q_SIZE_MAX (size_t)1000000
+
+#define EDGE_BATCH_MAX (size_t)1000
+
+#endif // SANDY_MTBFS_H
--- src/sandy.h
+++ src/sandy.h
@@ -4,13 +4,5 @@
 #define GRAPH_TABLE_NAME "graph"
 #define DIST_TABLE_NAME "dist"
 #define COORDINATOR_LOCATION "infrc:host=192.168.1.101,port=12246"
-#define REPORTER_NAP_TIME 10
-
-#define MULTIWRITE_REQ_MAX (size_t)400
-#define MULTIREAD_REQ_MAX (size_t)1000
-
-#define FRONTIER_EDGE_Q_SIZE_MAX (size_t)1000000
-
-#define EDGE_BATCH_MAX (size_t)1000
 
 #endif // SANDY_SANDY_H
