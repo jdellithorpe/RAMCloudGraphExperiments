cc8c735014ab56ac7e3218d2266b008029879f6a
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-05-30 09:27:41 -0700

    Added profiling information to the reader

--- src/reader.cc
+++ src/reader.cc
@@ -2,15 +2,72 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
+#include <boost/thread.hpp>
 #include <iostream>
 #include <fstream>
+#include "ShortMacros.h"
 #include "RamCloud.h"
+#include "Cycles.h"
 #include "sandy.h"
 #include "reader.h"
 #include "adjacency_list.pb.h"
 
 using namespace RAMCloud;
 using namespace Sandy::ProtoBuf;
+
+bool terminate = false;
+
+uint64_t stat_time_misc0_start;
+uint64_t stat_time_misc0_acc = 0;
+uint64_t stat_time_misc1_start;
+uint64_t stat_time_misc1_acc = 0;
+uint64_t stat_time_misc2_start;
+uint64_t stat_time_misc2_acc = 0;
+uint64_t stat_time_misc3_start;
+uint64_t stat_time_misc3_acc = 0;
+uint64_t stat_time_misc4_start;
+uint64_t stat_time_misc4_acc = 0;
+uint64_t stat_time_misc5_start;
+uint64_t stat_time_misc5_acc = 0;
+
+uint64_t node_write_count = 0;
+uint64_t bytes_written = 0;
+uint64_t stat_time_write_start;
+uint64_t stat_time_write_acc = 0;
+
+void report_stats() {
+  LOG(NOTICE, "Total time for program execution   : %f seconds", Cycles::toSeconds(Cycles::rdtsc() - stat_time_misc5_start));
+  LOG(NOTICE, "Total time for doing writes        : %f seconds", Cycles::toSeconds(stat_time_write_acc));
+  LOG(NOTICE, "Total number of nodes written      : %lu nodes", node_write_count);
+  LOG(NOTICE, "Total number of bytes written      : %lu bytes", bytes_written);
+  LOG(NOTICE, "Average number of bytes per node   : %lu bytes/node", bytes_written/node_write_count);
+  LOG(NOTICE, "Multiwrite average bandwidth (B/s) : %f B/s", bytes_written/Cycles::toSeconds(stat_time_write_acc));
+  LOG(NOTICE, "Multiwrite average bandwidth (N/s) : %f N/s", (float)node_write_count/Cycles::toSeconds(stat_time_write_acc));
+  LOG(NOTICE, "Multiwrite average latency (s/N)   : %f microseconds", Cycles::toSeconds(stat_time_write_acc)/float(node_write_count) * 1000000.0);
+  LOG(NOTICE, "Program average bandwidth (B/s)    : %f B/s", bytes_written/Cycles::toSeconds(Cycles::rdtsc() - stat_time_misc5_start));
+  LOG(NOTICE, "Program average bandwidth (N/s)    : %f N/s", node_write_count/Cycles::toSeconds(Cycles::rdtsc() - stat_time_misc5_start));
+  LOG(NOTICE, "Total time doing misc0: %f seconds", Cycles::toSeconds(stat_time_misc0_acc));
+  LOG(NOTICE, "Total time doing misc1: %f seconds", Cycles::toSeconds(stat_time_misc1_acc));
+  LOG(NOTICE, "Total time doing misc2: %f seconds", Cycles::toSeconds(stat_time_misc2_acc));
+  LOG(NOTICE, "Total time doing misc3: %f seconds", Cycles::toSeconds(stat_time_misc3_acc));
+  LOG(NOTICE, "Total time doing misc4: %f seconds", Cycles::toSeconds(stat_time_misc4_acc));
+  LOG(NOTICE, "Total time doing misc5: %f seconds", Cycles::toSeconds(stat_time_misc5_acc));
+}
+
+/**
+ * Main function of the reporter thread
+ * Reports some stats in fixed intervals of time.
+ **/
+void reporter() {
+  while(true) {
+    sleep(REPORTER_NAP_TIME);
+    if(terminate) 
+      return;
+    LOG(NOTICE, "-------------------- Reporter Output Start --------------------");
+    report_stats();
+    LOG(NOTICE, "-------------------- Reporter Output Stop --------------------");
+  }
+}
 
 int main(int argc, char* argv[]) {
   GOOGLE_PROTOBUF_VERIFY_VERSION;
@@ -70,6 +127,9 @@ int main(int argc, char* argv[]) {
 
   std::fstream graph_filestream(input_file, std::fstream::in);
 
+  // Start up the reporter thread
+  boost::thread reporter_thread(reporter);
+
   if(input_format == "edge_list") {
     if( ramcloud_format == "v2" ) {
       string edge_str;
@@ -126,21 +186,31 @@ int main(int argc, char* argv[]) {
     std::vector<uint64_t> adj_list_intvec[MULTIWRITE_REQ_MAX];
     string src_str[MULTIWRITE_REQ_MAX];
     uint64_t src_int[MULTIWRITE_REQ_MAX];
-    uint64_t node_write_count = 0;
-    uint64_t bytes_written = 0;
-    uint64_t stat_time_write_start;
-    uint64_t stat_time_write_acc = 0;
     size_t multiwrite_queue_size = 0;
     MultiWriteObject* requests[MULTIWRITE_REQ_MAX];
     Tub<MultiWriteObject> objects[MULTIWRITE_REQ_MAX];
-    while(std::getline(graph_filestream, adj_str)) {
+
+    stat_time_misc5_start = Cycles::rdtsc();
+    while(true) {
+      
+      stat_time_misc0_start = Cycles::rdtsc();
+      if(!std::getline(graph_filestream, adj_str))
+        break;
+      stat_time_misc0_acc += Cycles::rdtsc() - stat_time_misc0_start;    
+  
+      stat_time_misc1_start = Cycles::rdtsc();
       boost::split(adj_vec, adj_str, boost::is_any_of(" "));
+      stat_time_misc1_acc += Cycles::rdtsc() - stat_time_misc1_start;
       if(adj_vec.size() > 1) {
         if( ramcloud_format == "v4" ) {        
+          stat_time_misc2_start = Cycles::rdtsc();
           src_int[multiwrite_queue_size] = boost::lexical_cast<uint64_t>(adj_vec[0]);
-          
+          stat_time_misc2_acc += Cycles::rdtsc() - stat_time_misc2_start;          
+
           uint64_t append_int;
           append_int = (uint64_t)adj_vec.size()-1;
+          
+          stat_time_misc3_start = Cycles::rdtsc();
           adj_list_str[multiwrite_queue_size].clear();          
           adj_list_str[multiwrite_queue_size].append((char*)&append_int, sizeof(uint64_t));
 
@@ -148,14 +218,18 @@ int main(int argc, char* argv[]) {
             append_int = boost::lexical_cast<uint64_t>(adj_vec[i]);
             adj_list_str[multiwrite_queue_size].append((char*)&append_int, sizeof(uint64_t));
           }
+          stat_time_misc3_acc += Cycles::rdtsc() - stat_time_misc3_start;
 
+          stat_time_misc4_start = Cycles::rdtsc();
           objects[multiwrite_queue_size].construct( graph_tableid,
                                                     (char*)&src_int[multiwrite_queue_size],
                                                     sizeof(uint64_t),
                                                     adj_list_str[multiwrite_queue_size].c_str(),
                                                     adj_list_str[multiwrite_queue_size].length() );
+          stat_time_misc4_acc += Cycles::rdtsc() - stat_time_misc4_start;
         } else if( ramcloud_format == "v3" ) {
           src_int[multiwrite_queue_size] = boost::lexical_cast<uint64_t>(adj_vec[0]);
+          adj_list_pb.clear_neighbor();
           for(int i = 1; i<adj_vec.size(); i++) {
             adj_list_pb.add_neighbor(boost::lexical_cast<uint64_t>(adj_vec[i]));
           }
@@ -167,6 +241,7 @@ int main(int argc, char* argv[]) {
                                                     adj_list_str[multiwrite_queue_size].length() );
         } else if( ramcloud_format == "v2" ) {
           src_str[multiwrite_queue_size] = adj_vec[0];
+          adj_list_pb.clear_neighbor();
           for(int i = 1; i<adj_vec.size(); i++) {
             adj_list_pb.add_neighbor(boost::lexical_cast<uint64_t>(adj_vec[i]));
           }
@@ -208,10 +283,7 @@ int main(int argc, char* argv[]) {
           multiwrite_queue_size = 0;
         }
 
-        if(verbose && (node_write_count%100000)==0)
-          std::cout << prog_name << ": INFO: wrote " << node_write_count << "th vertex to RAMCloud, total of " << bytes_written << "B written @ " << bytes_written/Cycles::toSeconds(stat_time_write_acc) << "B/s, " << Cycles::toMicroseconds(stat_time_write_acc)/(double)node_write_count << "us/node\n";
       }
-      adj_list_pb.clear_neighbor();
     }
 
     if(multiwrite_queue_size != 0) {
@@ -224,11 +296,15 @@ int main(int argc, char* argv[]) {
 
       multiwrite_queue_size = 0;
     }
-
+    stat_time_misc5_acc += Cycles::rdtsc() - stat_time_misc5_start;
   } else {
     fprintf(stderr, "ERROR: Unrecognized value for input_format program option: %s\n", input_format.c_str());
     return -1;
   }
 
+  // Send terminate signal to the reporter thread
+  terminate = true;
+  reporter_thread.join();
+  
   return 0;
 }
