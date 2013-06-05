097f6797b569ef1455e188c4eb7b7485dfaf3e65
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-06-05 09:07:43 -0700

    Modified seconds_draft/sandy.h

--- src/second_draft/sandy.h
+++ src/second_draft/sandy.h
@@ -3,7 +3,7 @@
 
 #define GRAPH_TABLE_NAME "graph"
 #define DIST_TABLE_NAME "dist"
-#define COORDINATOR_LOCATION "infrc:host=192.168.1.101,port=12247"
+#define COORDINATOR_LOCATION "infrc:host=192.168.1.101,port=12246"
 
 #define MULTIWRITE_REQ_MAX (size_t)64
 #define MULTIREAD_REQ_MAX (size_t)8
