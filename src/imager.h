92b32aa9409fe31619ee5e3982a43e8a636a6bdd
Jonathan Ellithorpe <jdellit@stanford.edu>
2013-06-05 09:08:48 -0700

    Added imager.h

---
+++ src/imager.h
@@ -0,0 +1,8 @@
+#ifndef SANDY_IMAGER_H // <PROJECT>_<PATH>_<FILE>_H
+#define SANDY_IMAGER_H
+
+#define REPORTER_NAP_TIME 1
+
+#define WRITE_BUFFER_SIZE 512 // uint64_t integers
+
+#endif // SANDY_IMAGER_H
